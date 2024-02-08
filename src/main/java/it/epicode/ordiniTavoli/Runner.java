d5405ab546693d907abddceff74df281d068aa73
LorenzoN13 <lorenzoneroni13@gmail.com>
2024-02-08 17:57:02 +0100

    in corso

--- src/main/java/it/epicode/ordiniTavoli/Runner.java
+++ src/main/java/it/epicode/ordiniTavoli/Runner.java
@@ -49,28 +49,36 @@ public class Runner  implements CommandLineRunner {
         menuService.salvaMenu(menu);
 
         Drink water = ctx.getBean("water", Drink.class);
+        water.setMenu(menu);
         itemService.salvaItem(water);
 
         Drink cocaCola= ctx.getBean("cocaCola", Drink.class);
+        cocaCola.setMenu(menu);
         itemService.salvaItem(cocaCola);
 
         Topping mozzarella = ctx.getBean("mozzarella", Topping.class);
+        mozzarella.setMenu(menu);
         itemService.salvaItem(mozzarella);
 
         Topping pomodoro = ctx.getBean("pomodoro", Topping.class);
+        pomodoro.setMenu(menu);
         itemService.salvaItem(pomodoro);
 
         Topping salame = ctx.getBean("salame", Topping.class);
+        salame.setMenu(menu);
         itemService.salvaItem(salame);
 
         Topping prosciutto = ctx.getBean("prosciutto", Topping.class);
+        prosciutto.setMenu(menu);
         itemService.salvaItem(prosciutto);
 
         Pizza margherita = ctx.getBean("margherita", Pizza.class);
+        margherita.setMenu(menu);
         itemService.salvaItem(margherita);
 
 
         Pizza margheritaSalame = ctx.getBean("margheritaSalame", Pizza.class);
+        margheritaSalame.setMenu(menu);
         itemService.salvaItem(margheritaSalame);
 
 
